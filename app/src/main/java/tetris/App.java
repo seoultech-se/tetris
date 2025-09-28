8416344e04d93095333d478f4f98fbef72b51830
KKT <kimgt2828@gmail.com>
2025-09-28 18:49:18 +0900

    initial test

--- app/src/main/java/tetris/App.java
+++ app/src/main/java/tetris/App.java
@@ -5,7 +5,7 @@ package tetris;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello World! Test";
     }
 
     public static void main(String[] args) {
